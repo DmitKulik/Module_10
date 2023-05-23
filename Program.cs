5e135c3dd5873c5517ee4946116c868300f8faa1
Dmitry <grammattika@gmail.com>
2023-05-23 23:29:40 +0300

    определение интеофейса в приложении

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿namespace Module_10{
     internal class Program{
+
+        public interface IWriter { void Writer(); } // мое первое определение интерфейса в приложении
         static void Main(string[] args) { 
 
         }
