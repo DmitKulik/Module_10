3015d41eacbb7677673a99c6d0d7a6f731b4fc5a
Dmitry <grammattika@gmail.com>
2023-05-25 23:17:34 +0300

    явная оеализация интерфейса + коменты

--- Program.cs
+++ Program.cs
@@ -3,32 +3,41 @@
 namespace Module_10
 {
     internal class Program
-    {
-
-        public interface IWathApp
-        {
-            public void SendMessage(string message);
-        }
+    { 
+        // Определяется интерфейс IViber со методом SendMessage
         public interface IViber
         {
             public void SendMessage(string message);
         }
+        // Определяется интерфейс IWathApp со методом SendMessage
+        public interface IWathApp
+        {
+            public void SendMessage(string message);
+        }
+
         static void Main(string[] args)
         {
+            // Создается новый объект класса NewMessage
             NewMessage newMessage = new NewMessage();
 
+            // Вызывается метод SendMessage из интерфейса IWathApp
             ((IWathApp)newMessage).SendMessage("Test1");
+
+            // Вызывается метод SendMessage из интерфейса IViber
             ((IViber)newMessage).SendMessage("Test2");
         }
-        
     }
+
+    // Класс NewMessage реализует интерфейсы IWathApp и IViber
     public class NewMessage : IWathApp, IViber
     {
+        // Реализация метода SendMessage из интерфейса IWathApp
         void IWathApp.SendMessage(string message)
         {
             Console.WriteLine("{ 0 } : { 1 }", "WathApp", message);
         }
 
+        // Реализация метода SendMessage из интерфейса IViber
         void IViber.SendMessage(string message)
         {
             Console.WriteLine("{ 0 } : { 1 }", "Viber", message);
